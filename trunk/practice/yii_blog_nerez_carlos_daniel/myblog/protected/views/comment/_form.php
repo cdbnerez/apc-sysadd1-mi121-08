d166d5da92cec4a95d1dd7e2f3a4813ebc2e48a5
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-07-13 22:43:25 +0800

    updated the _form.php under trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/comment/_

--- trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/comment/_form.php
+++ trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/comment/_form.php
@@ -6,14 +6,10 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'comment-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form=$this->beginWidget('CActiveForm', array(
+		'id'=>'comment-form',
+		'enableAjaxValidation'=>true,
+	)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
