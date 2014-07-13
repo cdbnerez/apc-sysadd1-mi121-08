e4934ca69b9147e0cd39cee21acba7fa558144a3
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-07-14 00:21:47 +0800

    updated column2.php under trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/layouts/
    created a new folder (views) and php file (userMenu.php) under trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/components/

--- trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/layouts/column2.php
+++ trunk/practice/yii_blog_nerez_carlos_daniel/myblog/protected/views/layouts/column2.php
@@ -1,10 +1,12 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
+
 <div class="span-19">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
 </div>
+
 <div class="span-5 last">
 	<div id="sidebar">
 	<?php
@@ -19,4 +21,9 @@
 	?>
 	</div><!-- sidebar -->
 </div>
+
+<div id="sidebar">
+    <?php if(!Yii::app()->user->isGuest) $this->widget('UserMenu'); ?>
+</div>
+
 <?php $this->endContent(); ?>
