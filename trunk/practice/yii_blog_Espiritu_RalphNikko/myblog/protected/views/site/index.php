2c5a694648fc7c8e97e7b014f1cf86c2dd6998d4
Ralph Nikko Espiritu <pafbrat91@gmail.com>
2014-07-20 22:37:33 +0800

    Updated Yii Blog

--- trunk/practice/yii_blog_Espiritu_RalphNikko/myblog/protected/views/site/index.php
+++ trunk/practice/yii_blog_Espiritu_RalphNikko/myblog/protected/views/site/index.php
@@ -4,14 +4,15 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Philippians 4:13</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>I can do all things through Christ who strengthens me</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
+
 <ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+	<li><h1>Aviation:</h1></li>
+	
+	<li><h1>Music:</h1> <code><?php echo $this->getLayoutFile('main'); ?></code></li>
 </ul>
 
 <p>For more details on how to further develop this application, please read
