bac2e25a3f39b2c89bb9dc09730610c1e8db2d83
John Michael Santos <jhnmchlsantos@gmail.com>
2014-08-11 16:24:39 +0800

    updated database

--- trunk/application/rmc-wbos/prototype/protected/data/rmc-wbos.sql
+++ trunk/application/rmc-wbos/prototype/protected/data/rmc-wbos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2014 at 06:54 PM
+-- Generation Time: Aug 11, 2014 at 10:23 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -36,14 +36,15 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `cus_user_passwd` varchar(45) NOT NULL,
   `cus_contact_num` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `customer`
 --
 
 INSERT INTO `customer` (`id`, `cus_type`, `cus_company`, `cus_fname`, `cus_lname`, `cus_user_name`, `cus_user_passwd`, `cus_contact_num`) VALUES
-(1, '1', 'APC', 'Nikko', 'Espiritu', 'nikko123', 'nikko123', '0981231238');
+(1, '1', 'APC', 'Nikko', 'Espiritu', 'nikko123', 'nikko123', '0981231238'),
+(2, 'Retail', 'JM''s Sari-sari Store', 'John Michael', 'Santos', 'jmsantos', 'jm8322187', '09428032613');
 
 -- --------------------------------------------------------
 
@@ -60,14 +61,15 @@ CREATE TABLE IF NOT EXISTS `delivery` (
   `customer_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_delivery_customer1_idx` (`customer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `delivery`
 --
 
 INSERT INTO `delivery` (`id`, `del_add`, `del_city`, `del_country`, `del_zip`, `customer_id`) VALUES
-(4, '1234-A Juan Luna st, Paseo De Roxas', 'Makati', 'Philippines', '1000', 1);
+(4, '1234-A Juan Luna st, Paseo De Roxas', 'Makati', 'Philippines', '1000', 1),
+(5, '1442 Mactan Street Baclaran', 'Paranaque City', 'Philippines', '0710', 2);
 
 -- --------------------------------------------------------
 
@@ -80,14 +82,15 @@ CREATE TABLE IF NOT EXISTS `item_inventory` (
   `item_desc` varchar(100) NOT NULL,
   `item_price` decimal(10,0) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `item_inventory`
 --
 
 INSERT INTO `item_inventory` (`id`, `item_desc`, `item_price`) VALUES
-(1, 'Peanut Butter 200 grams', '500');
+(1, 'Peanut Butter 200 grams', '500'),
+(2, 'Marshmallow 5grams', '10');
 
 -- --------------------------------------------------------
 
@@ -108,14 +111,15 @@ CREATE TABLE IF NOT EXISTS `order` (
   KEY `fk_order_customer1_idx` (`customer_id`),
   KEY `fk_order_delivery1_idx` (`delivery_id`),
   KEY `fk_order_payment_method1_idx` (`payment_method_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `order`
 --
 
 INSERT INTO `order` (`id`, `order_date`, `order_total`, `payment_total`, `order_status`, `customer_id`, `delivery_id`, `payment_method_id`) VALUES
-(4, '9999-12-31', '500', '0', 'approved', 1, 4, 1);
+(4, '9999-12-31', '500', '0', 'approved', 1, 4, 1),
+(5, '0000-00-00', '21', '700', 'Pending', 2, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -131,14 +135,15 @@ CREATE TABLE IF NOT EXISTS `order_list` (
   PRIMARY KEY (`id`),
   KEY `fk_order_list_item_inventory1_idx` (`item_inventory_id`),
   KEY `fk_order_list_order1_idx` (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `order_list`
 --
 
 INSERT INTO `order_list` (`id`, `item_qty`, `item_inventory_id`, `order_id`) VALUES
-(2, 1, 1, 4);
+(2, 1, 1, 4),
+(3, 3, 2, 5);
 
 -- --------------------------------------------------------
 
@@ -159,14 +164,15 @@ CREATE TABLE IF NOT EXISTS `payment_method` (
   `payment_terms_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_payment_method_payment_terms1_idx` (`payment_terms_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `payment_method`
 --
 
 INSERT INTO `payment_method` (`id`, `payment_type`, `payment_desc`, `payment_method`, `card_no`, `cvc_no`, `card_type`, `bank_name`, `card_expire`, `payment_terms_id`) VALUES
-(1, 'I', 'Installments', 'Installments', 2147483647, 123, 'Credit', 'ABC Bank', '9999-12-31', 1);
+(1, 'I', 'Installments', 'Installments', 2147483647, 123, 'Credit', 'ABC Bank', '9999-12-31', 1),
+(2, '1', 'It is where you credit card to purchase', 'Credit Card', 12345, 7777, 'Credit Card', 'BDO', '0000-00-00', 2);
 
 -- --------------------------------------------------------
 
@@ -180,14 +186,15 @@ CREATE TABLE IF NOT EXISTS `payment_terms` (
   `pay_per_month` int(11) NOT NULL,
   `pay_discount` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `payment_terms`
 --
 
 INSERT INTO `payment_terms` (`id`, `pay_terms`, `pay_per_month`, `pay_discount`) VALUES
-(1, 5, 100, 0);
+(1, 5, 100, 0),
+(2, 1, 2500, 250);
 
 --
 -- Constraints for dumped tables
