8725e0dd9773005e45e26ca3620ed99bb2bcd9f4
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-08-26 18:56:01 +0800

    Edited the texts in the main page.
    trunk/application/rmc-wbos/prototype/protected/views/site/index.php

--- trunk/application/rmc-wbos/prototype/protected/views/site/index.php
+++ trunk/application/rmc-wbos/prototype/protected/views/site/index.php
@@ -5,7 +5,15 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1><i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<br>
+<h3><p align = center> Welcome to the Rodzon Marketing Corporation - Web Based Ordering System (RMC-WBOS)!</p></h2>
 
+<p align = justify> There are two versions of this web application. The first version is the localhost version and the other one is
+a hosted version. To access the hosted version, <a href="http://www.apc.csf.ph/rmc-wbos">click here</a>.</p>
+
+<p align = justify> To view the documentation of this project, <a href="http://www.projects2.apc.edu.ph/wiki"> click here </a>.</p> 
+ 
+<!--
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>This site aims to:</p>
@@ -18,3 +26,4 @@ $this->pageTitle=Yii::app()->name;
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p>
+-->
