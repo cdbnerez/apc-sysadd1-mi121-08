4c2f8a952f08cf45fb66875dc4e51fbfc5f4e8ff
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-10-13 10:29:57 +0800

    Customer view can now be accessed using PaymentMethod table. Partial code for payment_terms already appended on paymentMethod/view.php

--- trunk/application/rmc-wbos/prototype/protected/views/paymentMethod/view.php
+++ trunk/application/rmc-wbos/prototype/protected/views/paymentMethod/view.php
@@ -28,3 +28,43 @@ $this->menu=array(
 		'payment_terms_id',
 	),
 )); ?>
+
+<?php $conf= PaymentTerms::model()->findAll('payment_terms_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Customer Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+				'cus_type',
+				'cus_company',
+				'cus_fname',
+				'cus_lname',
+				'cus_user_name',
+				'cus_user_passwd',
+				'cus_contact_num',
+			),
+	)); ?>
+<br>
+
+<?php $conf= Customer::model()->findAll('id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Customer Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+				'cus_type',
+				'cus_company',
+				'cus_fname',
+				'cus_lname',
+				'cus_user_name',
+				'cus_user_passwd',
+				'cus_contact_num',
+			),
+	)); ?>
+<br>
+<?php }} ?>
+<?php }} ?>
