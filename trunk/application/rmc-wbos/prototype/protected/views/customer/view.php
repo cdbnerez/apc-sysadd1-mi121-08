f45b3b88d6768aea7d770b5a782a713275405822
John Michael Santos <jhnmchlsantos@gmail.com>
2014-10-27 23:12:51 +0800

    Added 'Add Order' method to the Customer view but still not yet functioning

--- trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
+++ trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
@@ -13,6 +13,7 @@ $this->menu=array(
 	array('label'=>'Update Customer', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Delete Customer', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Customer', 'url'=>array('admin')),
+	array('label'=>'Add Order', 'url'=>array('order')),
 );
 ?>
 <h1><?php echo $model->cus_company?><?php echo ' -'?> <?php echo $model->FullName?></h1>
