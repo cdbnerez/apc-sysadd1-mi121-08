a119bb014d9fed7ae407289e2bba0d76e7735e32
John Michael Santos <jhnmchlsantos@gmail.com>
2014-10-29 22:37:27 +0800

    It formats the Order table - dropdown and datefield

--- trunk/application/rmc-wbos/prototype/protected/views/order/_form.php
+++ trunk/application/rmc-wbos/prototype/protected/views/order/_form.php
@@ -1,42 +1,41 @@
-<?php
-/* @var $this OrderController */
-/* @var $model Order */
-/* @var $form CActiveForm */
-?>
-
-<div class="form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'order-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
+<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'order_date'); ?>
-		<?php echo $form->textField($model,'order_date'); ?>
+		
+		<?php echo CHtml::activeTextField($model,'order_date',array("id"=>"order_date")); ?>
+		<?php $this->widget('application.extensions.calendar.SCalendar',
+        array(
+        'inputField'=>'order_date',
+        'ifFormat'=>'%Y-%m-%d',
+		));
+		?>
+		
 		<?php echo $form->error($model,'order_date'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'order_status'); ?>
-		<?php echo $form->textField($model,'order_status',array('size'=>45,'maxlength'=>45)); ?>
+		
+		<?php echo $form->dropDownList($model,'order_status',array("Approved"=>"Approved", "Denied"=>"Denied", "Pending"=>"Pending")
+		,array('empty'=>'Select Order Status')); ?>
+		
 		<?php echo $form->error($model,'order_status'); ?>
 	</div>
 
 	<div class="row">
+	
 		<?php echo $form->labelEx($model,'customer_id'); ?>
-		<?php echo $form->textField($model,'customer_id'); ?>
+		
+		<?php echo $form->dropDownList($model, 'customer_id', CHtml::listData(
+		Customer::model()->findAll(), 'id', 'FullName'),
+		array('prompt' => 'Select a Customer Name')
+		); ?>
+		
 		<?php echo $form->error($model,'customer_id'); ?>
+	
 	</div>
-
+	
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
