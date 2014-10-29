b2452cb4a86c3176a6eb83bacf5f9a3e1f2180d3
John Michael Santos <jhnmchlsantos@gmail.com>
2014-10-29 23:23:51 +0800

    Change the _form and view of the Payment Method table

--- trunk/application/rmc-wbos/prototype/protected/views/paymentMethod/_form.php
+++ trunk/application/rmc-wbos/prototype/protected/views/paymentMethod/_form.php
@@ -18,28 +18,51 @@
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
+	
+	<div class="row">
+		<?php echo $form->labelEx($model,'order_id'); ?>
+		
+		<?php echo $form->dropDownList($model, 'order_id', CHtml::listData(
+		Order::model()->findAll(), 'id', 'id'),
+		array('prompt' => 'Select a Order No#')
+		); ?>
+		
+		<?php echo $form->error($model,'order_id'); ?>
+	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'payment_type'); ?>
-		<?php echo $form->textField($model,'payment_type',array('size'=>5,'maxlength'=>5)); ?>
+		
+		<?php echo $form->dropDownList($model,'payment_type',array("CASH"=>"Cash", "Cheque"=>"Cheque")
+		,array('empty'=>'Select Payment Type')); ?>
+		
 		<?php echo $form->error($model,'payment_type'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'payment_desc'); ?>
-		<?php echo $form->textField($model,'payment_desc',array('size'=>45,'maxlength'=>45)); ?>
+		
+		<?php echo $form->dropDownList($model,'payment_desc',array("Cash (Straight)"=>"Cash (Straight)", "Cash (Cash Upon Delivery)"=>"Cash (Cash Upon Delivery)", "Cheque"=>"Cheque", "Cash (Terms)"=>"Cash (Terms)")
+		,array('empty'=>'Select Payment Description')); ?>
+		
 		<?php echo $form->error($model,'payment_desc'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'bank_name'); ?>
-		<?php echo $form->textField($model,'bank_name',array('size'=>45,'maxlength'=>45)); ?>
+		
+		<?php echo $form->dropDownList($model,'bank_name',array("BDO"=>"Banco De Oro", "Robinsons"=>"Robinsons Bank", "BPI"=>"Bank of the Philippine Islands")
+		,array('empty'=>'Select Bank Name')); ?>
+		
 		<?php echo $form->error($model,'bank_name'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'payment_terms'); ?>
-		<?php echo $form->textField($model,'payment_terms'); ?>
+		
+		<?php echo $form->dropDownList($model,'payment_terms',array(0=>"Straight Payment", 30=>"30 Days", 60=>"60 Days", 90 => "60 Days")
+		,array('empty'=>'Select Bank Name')); ?>
+		
 		<?php echo $form->error($model,'payment_terms'); ?>
 	</div>
 
@@ -59,12 +82,6 @@
 		<?php echo $form->labelEx($model,'payment_total_amount'); ?>
 		<?php echo $form->textField($model,'payment_total_amount',array('size'=>10,'maxlength'=>10)); ?>
 		<?php echo $form->error($model,'payment_total_amount'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'order_id'); ?>
-		<?php echo $form->textField($model,'order_id'); ?>
-		<?php echo $form->error($model,'order_id'); ?>
 	</div>
 
 	<div class="row buttons">
