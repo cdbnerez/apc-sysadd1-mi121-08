95d6af961a22583eb4cac4ccad6f0b573de4e86f
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-10-30 00:15:40 +0800

    updated customer/view.php and added some values onto the rmc-wbos database

--- trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
+++ trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
@@ -31,3 +31,71 @@ $this->menu=array(
 		'cus_contact_num',
 	),
 )); ?>
+
+<?php $conf= Delivery::model()->findAll('order_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Delivery Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   	
+			    'del_add',
+			    'del_city',
+			    'del_country',
+			    'del_zip',
+			    //'customer_id' => 'Customer Name'
+			    //array('label'=>'Person', 'value'=>$model->person->FullName),
+			),
+	)); ?>
+<br>
+<?php }} ?>
+
+<?php $conf= PaymentMethod::model()->findAll('order_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Payment Method Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+				'payment_type',
+				'bank_name',
+				'payment_desc',
+			),
+	)); ?>
+<br>
+<?php }} ?>
+
+<?php $conf= Order::model()->findAll('customer_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Order Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+			    'order_date',
+				'order_status',
+			    
+			),
+	)); ?>
+<br>
+<?php }} ?>
+
+<?php $conf= OrderList::model()->findAll('order_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>OrderList Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+				'item_qty',
+				array('label'=>'Item Description', 'value'=>$row->item->item_desc),
+				//'item_id',
+				'order_list_total_amount',
+			),
+	)); ?>
+<br>
+<?php }} ?>
