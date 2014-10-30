34cd9ab16df4701688699bbdc5086c9ae991ac85
John Michael Santos <jhnmchlsantos@gmail.com>
2014-10-30 23:46:17 +0800

    Edit the index.php to show the changes for the Mobile css

--- trunk/application/rmc-wbos/prototype/protected/views/site/index.php
+++ trunk/application/rmc-wbos/prototype/protected/views/site/index.php
@@ -6,13 +6,32 @@ $this->pageTitle=Yii::app()->name;
 
 <h1><i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 <br>
-<h3><p align = center> Welcome to the Rodzon Marketing Corporation - Web Based Ordering System (RMC-WBOS)!</p></h2>
+<?php
+require_once 'Mobile_Detect.php';
+$detect = new Mobile_Detect;
+	
+	if ($detect->isMobile() ) {
+	echo '<h1><p align = center> Welcome to the Rodzon Marketing Corporation - Web Based Ordering System (RMC-WBOS)!</p></h12>';
+	}else{
+	echo '<h3><p align = center> Welcome to the Rodzon Marketing Corporation - Web Based Ordering System (RMC-WBOS)!</p></h3>';
+	}
+	?>
 
 <p align = justify> There are two versions of this web application. The first version is the localhost version and the other one is
 a hosted version. To access the hosted version, <a href="http://apc.csf.ph/rmc-wbos/">click here</a>.</p>
 
 <p align = justify> To view the documentation of this project, 
 <a href="http://projects2.apc.edu.ph/wiki/index.php/SYSADD1_MI121_Group_8:_Carlos_Daniel_Nerez_-_BSIT-MI"> click here </a>.</p> 
+
+<?php
+	require_once 'Mobile_Detect.php';
+	$detect = new Mobile_Detect;
+	
+	if ($detect->isMobile() ) {
+	echo '<p border = "10px"> MOBILE </p>';
+	echo '<p><font color = "red"> Mobile CSS Version </font></p>';
+	}
+	?>
  
 <!--
 <p>Congratulations! You have successfully created your Yii application.</p>
