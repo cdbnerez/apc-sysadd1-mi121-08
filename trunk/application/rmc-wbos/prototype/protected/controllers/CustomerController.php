18ec9eb6133eb9ddbe89ed9070d12485d8555eb8
John Michael Santos <jhnmchlsantos@gmail.com>
2014-10-30 13:36:13 +0800

    The cdbnerez admin account can manage and create a customer from the Customer table

--- trunk/application/rmc-wbos/prototype/protected/controllers/CustomerController.php
+++ trunk/application/rmc-wbos/prototype/protected/controllers/CustomerController.php
@@ -31,14 +31,22 @@ class CustomerController extends Controller
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+			/*array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
 				'users'=>array('@'),
-			),
+			),*/
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
 			),
+			
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update','admin','delete'),
+				'users'=>array('@'),
+				'expression'=>'isset(Yii::app()->user->type) &&
+					((Yii::app()->user->type==="Sytem Admin"))'
+			),
+			
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
