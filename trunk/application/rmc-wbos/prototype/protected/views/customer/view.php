4e4e6000f9c7fe0e6c10844ebfddf3d0521c22b8
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-11-04 13:08:39 +0800

    update on Customer's "Operation" functionalities

--- trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
+++ trunk/application/rmc-wbos/prototype/protected/views/customer/view.php
@@ -9,11 +9,17 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
+	/**
 	array('label'=>'List Customer', 'url'=>array('index')),
 	array('label'=>'Create Customer', 'url'=>array('create')),
 	array('label'=>'Update Customer', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Delete Customer', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Customer', 'url'=>array('admin')),
+	**/
+	
+	array('label'=>'Create Order for ' .$model->FullName, 'url'=>array('order/create','customer_id'=>$model->id)),
+	
+	
 );
 ?>
 
