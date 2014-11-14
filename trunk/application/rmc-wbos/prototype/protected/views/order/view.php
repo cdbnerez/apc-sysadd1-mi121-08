c4166c75434af71d71d4636b829bf76ab1c99b6f
Carlos Daniel B. Nerez <cdbnerez@gmail.com>
2014-11-14 23:14:48 +0800

    updated the order view

--- trunk/application/rmc-wbos/prototype/protected/views/order/view.php
+++ trunk/application/rmc-wbos/prototype/protected/views/order/view.php
@@ -23,7 +23,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Order #<?php echo $model->id; ?></h1>
+<h1>Order #<?php echo $model->id; ?> Information</h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
@@ -40,15 +40,58 @@ $this->menu=array(
 <?php if (count($conf) !== 0){?>
 <br>
 <h2>Order List Information</h2>
-<?php foreach ($conf as $row2) { ?>
-<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/update.png" align="right"/>', array('OrderList/update', 'id'=>$row2->id)); ?>
+<?php foreach ($conf as $row) { ?>
+<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/update.png" align="right"/>', array('OrderList/update', 'id'=>$row->id)); ?>
 <?php $this->widget('zii.widgets.CDetailView', array(
-	        'data'=>$row2,
+	        'data'=>$row,
 	        'attributes'=>array(   
 				'order_id',
 				'item_qty',
-				array('label'=>'Item Description', 'value'=>$row2->item->item_desc),
+				array('label'=>'Item Description', 'value'=>$row->item->item_desc),
 				'item_order_total',
+			),
+	)); ?>
+<br>
+<?php }} ?>
+
+<?php $conf= PaymentMethod::model()->findAll('order_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Payment Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/update.png" align="right"/>', array('paymentMethod/update', 'id'=>$row->id)); ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   
+				'order_id',
+				'payment_type',
+				'payment_desc',
+				'bank_name',
+				'payment_desc',
+				'payment_terms',
+			    'payment_per_month',
+			    'payment_discount',
+			    'payment_total_amount',
+			),
+	)); ?>
+<br>
+<?php }} ?>
+
+<?php $conf= Delivery::model()->findAll('order_id = :a', array(':a'=>$model->id));?>
+<?php if (count($conf) !== 0){?>
+<br>
+<h2>Delivery Information</h2>
+<?php foreach ($conf as $row) { ?>
+<?php echo CHtml::link('<img src="' . Yii::app()->request->baseUrl . '/images/update.png" align="right"/>', array('delivery/update', 'id'=>$row->id)); ?>
+<?php $this->widget('zii.widgets.CDetailView', array(
+	        'data'=>$row,
+	        'attributes'=>array(   	
+			    'del_add',
+			    'del_city',
+			    'del_country',
+			    'del_zip',
+			    //'customer_id' => 'Customer Name'
+			    //array('label'=>'Person', 'value'=>$model->person->FullName),
 			),
 	)); ?>
 <br>
